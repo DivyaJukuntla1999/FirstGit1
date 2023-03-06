fb04bb1eb3803e9a04eeef66eb0f6a3704ea624b
DivyaJukuntla1999 <126849699+DivyaJukuntla1999@users.noreply.github.com>
2023-03-07 00:58:31 +0530

    task5_firstgit

--- firstgit/demo_js_task3_dom.js
+++ firstgit/demo_js_task3_dom.js
@@ -12,3 +12,9 @@ var listItems = document.querySelectorAll("#items li");
 for (var i = 0; i < listItems.length; i++) {
   listItems[i].style.fontWeight = "bold";
 }
+const newItem = document.createElement('li');
+newItem.textContent = 'New Item';
+newItem.classList.add('list-group-item-new'); // add a new class to the element
+document.getElementById('items').appendChild(newItem);
+const newItem = document.getElementsByClassName('list-group-item-new')[0];
+newItem.style.backgroundColor = 'green';
