92590fd01addcfd017eac9170456347e8286c772
DivyaJukuntla1999 <126849699+DivyaJukuntla1999@users.noreply.github.com>
2023-03-07 00:40:53 +0530

    task_4_firstgit

--- firstgit/demo_js_task3_dom.js
+++ firstgit/demo_js_task3_dom.js
@@ -6,3 +6,9 @@ console.log(document.URL);
 const addItemsHeading = document.querySelector(".title");
 addItemsHeading.style.fontWeight = "bold";
 addItemsHeading.style.color = "green";
+var thirdElement = document.querySelectorAll("#items li")[2];
+thirdElement.style.backgroundColor = "green";
+var listItems = document.querySelectorAll("#items li");
+for (var i = 0; i < listItems.length; i++) {
+  listItems[i].style.fontWeight = "bold";
+}
