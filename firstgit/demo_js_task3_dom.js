1ee43cc6793ce3dc332d353e639e145125bd1976
DivyaJukuntla1999 <126849699+DivyaJukuntla1999@users.noreply.github.com>
2023-03-07 00:13:13 +0530

    firstgit_task3_done

---
+++ firstgit/demo_js_task3_dom.js
@@ -0,0 +1,8 @@
+console.dir(document);
+console.log('hi');
+console.log(document.doctype);
+console.log(document.forms);
+console.log(document.URL);
+const addItemsHeading = document.querySelector(".title");
+addItemsHeading.style.fontWeight = "bold";
+addItemsHeading.style.color = "green";
