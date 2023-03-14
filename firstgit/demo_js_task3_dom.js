5044f76a72a343fa228669a0b28a359a857e43b3
DivyaJukuntla1999 <126849699+DivyaJukuntla1999@users.noreply.github.com>
2023-03-14 19:37:31 +0530

    FirstGit_task7

--- firstgit/demo_js_task3_dom.js
+++ firstgit/demo_js_task3_dom.js
@@ -12,13 +12,20 @@ var listItems = document.querySelectorAll("#items li");
 for (var i = 0; i < listItems.length; i++) {
   listItems[i].style.fontWeight = "bold";
 }
-//const newItem = document.createElement('li');
-//newItem.textContent = 'New Item';
-//newItem.classList.add('list-group-item-new'); // add a new class to the element
-//document.getElementById('items').appendChild(newItem);
+const newItem = document.createElement('li');
+newItem.textContent = 'New Item';
+newItem.classList.add('list-group-item-new'); // add a new class to the element
+document.getElementById('items').appendChild(newItem);
 const newItem = document.getElementsByClassName('list-group-item-new')[0];
 newItem.style.backgroundColor = 'green';
 var items = document.getElementById('items').getElementsByTagName('li');
 items[1].style.backgroundColor = 'green';
 var items = document.getElementById('items').getElementsByTagName('li');
 items[2].style.display = 'none';
+const headerTitle = document.querySelector('#header-title');
+headerTitle.parentElement.insertBefore(document.createTextNode('Hello world '), headerTitle);
+const items = document.querySelector('#items');
+items.firstElementChild.parentElement.insertBefore(document.createTextNode('Hello world '), items.firstElementChild);
+console.log(itemList.children);
+console.log(itemList.children[1]);
+itemList.children[1].style.background = 'yellow';
